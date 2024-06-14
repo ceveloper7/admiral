41cebce12d0a60fd367273ce07e3b03c0d7854a5
Carlos V <cevabarcvilla@gmail.com>
2024-06-14 02:14:37 -0300

    The Admiral Erp project - Building kernel component

---
+++ kernel/admiral-kernel-util/src/main/java/com/admiral/kernel/util/App.java
@@ -0,0 +1,13 @@
+package com.admiral.kernel.util;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
